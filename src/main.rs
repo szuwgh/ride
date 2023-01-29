2e9bda9c32d7fe8693a91398bb8c24d97e618eb9
szuwgh <895340293@qq.com>
2023-01-29 16:09:14 +0800

    init project

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use gtk4::prelude::*;
+use gtk4::{Application, ApplicationWindow};
+use gtk4::Button;
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+
+fn main() {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+        // Create a button with label and margins
+        let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(|button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
+}
